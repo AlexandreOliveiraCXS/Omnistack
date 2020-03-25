33a55a9705313d460d8e942275dedfe7643c113b
Alexandre Custodio de Oliveira <alexandre.c.oliveira@hotmail.com>
2020-03-25 18:13:46 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,20 @@
+connection = require('../database/connection');
+
+module.exports = {
+    async create(req, res) {
+        const { id } = req.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if (!ong) {
+            return res.status(400).json({
+                error: 'Não há ongs com este ID'
+            });
+        }
+
+        return res.json(ong);
+    }
+}
